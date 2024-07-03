5957a7bc44d223a5159f0af51b49672e7353b4c7
yujinwu <yujinwu@tencent.com>
2024-07-03 17:30:05 +0800

    add comment

--- shapley_attribution_wf/shapley_attribution_ads.py
+++ shapley_attribution_wf/shapley_attribution_ads.py
@@ -13,28 +13,35 @@ from tqdm import tqdm
 
 class SimplifiedShapleyAttributionModel:
     """
-    simplyfied shapley shapley_attribution_wf method for ads.
-    R(S) is used to replace original shapley formular.
+    实现类似广告投放场景的渠道效用shapley归因计算.
     """
-    def __init__(self, journeys):
+    def __init__(self, journeys: [list[list]]):
+        """
+        :param journeys: 用户访问渠道记录，每个子列表代表一个用户访问的所有渠道。
+        """
+        # 从用户访问记录中获取所有渠道编号
         self.channels = set(chain(*journeys))
+        # 统计每种访问渠道组合数量
         self.journeys = Counter([frozenset(journey) for journey in journeys])
 
-    def _phi(self, channel_index):
+    def _phi(self, channel_index: int):
         """
-        :param channel_index: channel number
-        :return: shapley_attribution_wf
+        功能：实现单个shapley value计算
+        :param channel_index: 渠道的编号
+        :return: 对应渠道的shapley value
         """
+        # 获取所有包含渠道channel_index的访问记录
         s_channel = [k for k in self.journeys.keys() if channel_index in k]
+        # 使用简化的shapley公式计算
         score = 0
-        print(f"Computing phi for channel {channel_index}...")
         for S in tqdm(s_channel):
             score += self.journeys[S] / len(S)
-        print(f"Attribution score for channel {channel_index}: {score:.2f}")
-        print()
         return score
 
     def attribute(self):
+        """
+        :return: 返回所有渠道shapley value值
+        """
         print("Running Simplified Shapley Attribution Model...")
         print(f"Found {len(self.channels)} unique channels!")
         print("Computing journey statistics...")
@@ -45,61 +52,70 @@ class SimplifiedShapleyAttributionModel:
 
 class OrderedShapleyAttributionModel:
     """
-    ordered shapley shapley_attribution_wf method
+    考虑有序情况的广告渠道shapley value计算
     """
     def __init__(self, journeys: list[list]):
         """
-        :param journeys: visited channels for every user
+        :param journeys: 用户访问渠道记录，每个子列表代表一个用户访问的所有渠道
         """
         self.channels = set(chain(*journeys))
+        # 得到所有渠道组合
         self.p_power = list(self.powerset(self.channels))
         self.journeys = journeys
-        self.N = max([len(journey) for journey in self.journeys])  # get the max number of visiting channels by one user
+        # 获取用户最多访问的渠道个数
+        self.N = max([len(journey) for journey in self.journeys])
+        # 对访问不同渠道个数的用户进行归类，存成字典，key代表访问不同渠道的个数，value存储对应的所有访问渠道记录的列表，每个元素对应一个用户
         self.indexed_journeys = {
             i: [(s, set(s)) for s in self.journeys if len(set(s)) == i]
             for i in range(1, len(self.channels) + 1)
         }
-    def powerset(self, x):
+
+    def powerset(self, channel_index: int):
         """
-        :param x: channel indexs
-        :return: combination samples
+        :param channel_index: 渠道号
+        :return: 所有组合samples
         """
-        s = list(x)
+        s = list(channel_index)
         return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
-    def _r(self, s, channel_index, touchpoint_index):
+    def _r(self, channels_combination: set, channel_index: int, touchpoint_index: int):
         """
-        calculate the shapley value given a set s and a fixed channel and a fixed order number
-        :param s: one subset of total channels
-        :param channel_index: channnel index
-        :param touchpoint_index: the channel order in any channels set
-        :return: shapley value given a set s and a fixed channel and a fixed order number
+        根据给定的渠道组合、渠道号、访问次序，计算均分后的访问效用
+        :param channels_combination: 一种渠道组合
+        :param channel_index: 渠道号
+        :param touchpoint_index: 渠道次序
+        :return: 某个渠道组合、渠道号、访问次数的访问效用值
         """
         return sum(
             [
-                1 / journey.count(channel_index)  # if one channel appears in multiple touchpoints, evenly distirbute
+                # 同一个渠道出现在一个用户访问记录中的多个位置，采用均分方式计算
+                1 / journey.count(channel_index)
                 if (
-                    (s == journey_set)
+                    # 找到channels_combination对应的访问记录
+                    (channels_combination == journey_set)
+                    # 找到渠道号对应的访问次序，即touchpoint_index
                     and (journey[touchpoint_index - 1] == channel_index)
                 )
                 else 0
-                for journey, journey_set in self.indexed_journeys[len(s)]
+                for journey, journey_set in self.indexed_journeys[len(channels_combination)]
                 if touchpoint_index <= len(journey)
             ]
         )
 
-    def _phi(self, channel_index, touchpoint_index):
+    def _phi(self, channel_index: int, touchpoint_index: int):
         """
-        calculate shapley_attribution_wf for one channel and one order
-        :param channel_index: the select channel number
-        :param touchpoint_index: the order number of this channel
-        :return:
+        给定渠道号、访问次序，计算shapley value
+        :param channel_index: 渠道号
+        :param touchpoint_index: 访问次序
+        :return:shapley value
         """
+        # 获取包含channel_index渠道的所有组合samples
         s_all = [set(S) for S in self.p_power if channel_index in S]
         score = 0
         print(
             f"Computing phi for channel {channel_index}, touchpoint {touchpoint_index}..."
         )
+        # 计算shapley value
         for S in tqdm(s_all):
             score += self._r(S, channel_index, touchpoint_index) / len(S)
         print(
@@ -110,7 +126,7 @@ class OrderedShapleyAttributionModel:
 
     def attribute(self):
         """
-        calculate all channels's shapley_attribution_wf
+        计算所有渠道shapley value
         """
         print("Running Ordered Shapley Attribution Model...")
         print(f"Found {len(self.channels)} unique channels!")
--- shapley_attribution_wf/shapley_attribution_formula.py
+++ shapley_attribution_wf/shapley_attribution_formula.py
@@ -14,53 +14,60 @@ from tqdm import tqdm
 
 
 class ShapleyAttributionModelFormula:
-    '''
-    shapley shapley_attribution_wf for pre-set formulas
-    :param data_before: list data for comparent pre-time
-    :param data_after: list data for comparent after-time
-    '''
+    """
+    给定指标公式后计算因子的shapley value
+    """
 
-    def __init__(self, data_before: list(), data_after: list(), func):
-        '''
-        :param data_before: list data for comparent pre-time
-        :param data_after: list data for comparent after-time
-        '''
+    def __init__(self, data_before: list, data_after: list, func):
+        """
+        :param data_before: 前一时间段的因子取值列表
+        :param data_after: 后一时间段的因子取值列表
+        :param func: 指标表达式
+        """
         assert len(data_before) == len(data_after)
         self.data_before = np.array(data_before.copy())
         self.data_after = np.array(data_after.copy())
-        self.factors = set(range(len(data_before)))
+        # 因子编号
+        self.factors = list(range(len(data_before)))
         self.func = func
-        # 枚举组合
-        self.journeys = list(self.powerset(self.factors))
+        # 枚举因子组合
+        self.combinations = list(self.powerset(self.factors))
 
-    def powerset(self, x):
-        '''
-        :param x: channel indexs
-        :return: combination samples
-        '''
-        s = list(x)
-        return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+    def powerset(self, factors: list):
+        """
+        枚举组合samples
+        :param factors: 所有因子编号
+        :return: 组合samples
+        """
+        assert type(factors) == list
+        return chain.from_iterable(combinations(factors, r) for r in range(len(factors) + 1))
 
-    def _phi(self, channel_index):
-        '''
-        :param channel_index: factor number
-        :return: shapley values for a factor
-        '''
-        s_combination = [k for k in self.journeys if channel_index in k]
+    def _phi(self, factor_index):
+        """
+        单因子shapley value计算
+        :param factor_index: 因子编号
+        :return: 单因子shapley value
+        """
+        # 获取包含给定因子的组合samples
+        s_combination = [k for k in self.combinations if factor_index in k]
         score = 0
-        print(f"Computing phi for factor {channel_index}...")
+        print(f"Computing phi for factor {factor_index}...")
         for s in tqdm(s_combination):
             v_before = self.data_before.copy()
             v_after = self.data_before.copy()
-            v_before[list(set(s) - set([channel_index]))] = self.data_after[list(set(s) - set([channel_index]))]
+            v_before[list(set(s) - set([factor_index]))] = self.data_after[list(set(s) - set([factor_index]))]
             v_after[list(s)] = self.data_after[list(s)]
+            # shapley value计算原始公式
             score += math.factorial(len(s) - 1) * math.factorial(len(self.factors) - len(s)) \
-                     / math.factorial(len(self.factors)) * (self.func(*v_after) - self.func(*v_before))
-        print(f"Attribution score for foctor {channel_index}: {score:.2f}")
+                / math.factorial(len(self.factors)) * (self.func(*v_after) - self.func(*v_before))
+        print(f"Attribution score for foctor {factor_index}: {score:.2f}")
         print()
         return score
 
     def attribute(self):
+        """
+        :return:所有因子shapley value计算
+        """
         print("Running Simplified Shapley Attribution Model...")
         print(f"Found {len(self.factors)} unique factors!")
         print("Computing combinations...")
